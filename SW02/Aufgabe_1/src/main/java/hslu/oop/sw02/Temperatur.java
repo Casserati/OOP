b32989d0cc0f2573003e9f384e8f0eb9b2c5d30a
Casserati <francesco02@hotmail.ch>
2025-09-23 14:38:51 +0200

    added validation for to high and low kelvin and added unit tests

--- SW02/Aufgabe_1/src/main/java/hslu/oop/sw02/Temperatur.java
+++ SW02/Aufgabe_1/src/main/java/hslu/oop/sw02/Temperatur.java
@@ -2,60 +2,90 @@ package hslu.oop.sw02;
 
 public class Temperatur {
 
-    double temperatureInCelsius;
+    float temperatureInCelsius;
 
     /**
      * Default Constructor
      * sets default temperature to 20 degree Celsius
      */
     public Temperatur() {
-        this.temperatureInCelsius = 20.0d;
+        this.temperatureInCelsius = 20.0f;
     }
 
     /**
      * Constructor
-     * sets default temperature to given double value degree Celsius
+     * sets default temperature to given float value degree Celsius
      */
-    public Temperatur(double temperatureInCelsius) {
-        this.temperatureInCelsius = temperatureInCelsius;
+    public Temperatur(float temperatureInCelsius) {
+        if (verifyTemperatureValid(temperatureInCelsius)){
+            this.temperatureInCelsius = temperatureInCelsius;
+        }
     }
 
-    public double getTemperatureInCelsius() {
+    public float getTemperatureInCelsius() {
         return temperatureInCelsius;
     }
 
-    public void setTemperatureInCelsius(double temperatureInCelsius) {
-        this.temperatureInCelsius = temperatureInCelsius;
+    public void setTemperatureInCelsius(float temperatureInCelsius) {
+        if (verifyTemperatureValid(temperatureInCelsius)) {
+            this.temperatureInCelsius = temperatureInCelsius;
+        }
     }
 
     /**
      * @return current temperature of class in Kelvin
      */
-    public double getTemperatureInKelvin(){
-        return temperatureInCelsius  + 273.15d;
+    public float getTemperatureInKelvin() {
+        return fromCelsiusToKelvin(temperatureInCelsius);
     }
 
     /**
      * @return current temperature of class in Fahrenheit
      */
-    public double getTemperatureInFahrenheit(){
-        return temperatureInCelsius * 1.8d + 32d;
+    public float getTemperatureInFahrenheit() {
+        return fromCelsiusToFahrenheit(temperatureInCelsius);
     }
 
     /**
      * changes temperature relative to given value
+     *
      * @param temperatureInKelvin relative temperature change in Kelvin
      */
-    public void changeTemperatureInKelvin(double temperatureInKelvin){
-        this.temperatureInCelsius =- temperatureInKelvin - 273.15d;
+    public void changeTemperatureInKelvin(float temperatureInKelvin) {
+        if (verifyTemperatureValid(this.temperatureInCelsius + fromKelvinToCelsius(temperatureInKelvin))) {
+            this.temperatureInCelsius =+ temperatureInKelvin - 273.15f;
+        }
     }
 
     /**
      * changes temperature relative to given value
+     *
      * @param temperatureInCelsius relative temperature change in Celsius
      */
-    public void changeTemperatureInCelsius(double temperatureInCelsius){
-        this.temperatureInCelsius =- temperatureInCelsius;
+    public void changeTemperatureInCelsius(float temperatureInCelsius) {
+        if(verifyTemperatureValid(this.temperatureInCelsius + temperatureInCelsius)) {
+            this.temperatureInCelsius =+ temperatureInCelsius;
+        }
     }
 
+    private boolean verifyTemperatureValid(float value) {
+        if (fromCelsiusToKelvin(value) > 1000f || fromCelsiusToKelvin(value) < 0f) {
+            System.out.println("Temperatur ist nicht gültig für den gegebenen Wertebereich!");
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private float fromCelsiusToFahrenheit(float temperatureInCelsius) {
+        return temperatureInCelsius * 1.8f + 32f;
+    }
+
+    private float fromCelsiusToKelvin(float temperatureInCelsius){
+        return temperatureInCelsius + 273.15f;
+    }
+
+    private float fromKelvinToCelsius(float temperatureInKelvin){
+        return temperatureInKelvin - 273.15f;
+    }
 }
---
+++ SW02/Aufgabe_1/src/test/java/hslu/oop/sw02/TemperaturTest.java
@@ -0,0 +1,63 @@
+package hslu.oop.sw02;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class TemperaturTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private final String errorMessage = "Temperatur ist nicht gültig für den gegebenen Wertebereich!\r\n";
+
+    @BeforeEach
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(originalOut);
+    }
+
+    @Test
+    void setToOver1000Kelvin() {
+        Temperatur temperatur = new Temperatur(500);
+
+        temperatur.changeTemperatureInKelvin(1000);
+
+
+        assertEquals(errorMessage, outContent.toString());
+    }
+
+    @Test
+    void initTempOver1000Kelvin() {
+        Temperatur temperatur = new Temperatur(1000);
+
+        assertEquals(errorMessage, outContent.toString());
+    }
+
+    @Test
+    void setToUnder0Kelvin() {
+        Temperatur temperatur = new Temperatur(500);
+
+        temperatur.changeTemperatureInKelvin(-1000);
+
+
+        assertEquals(errorMessage, outContent.toString());
+    }
+
+    @Test
+    void initTempUnder0Kelvin() {
+        Temperatur temperatur = new Temperatur(-500);
+
+        assertEquals(errorMessage, outContent.toString());
+    }
+}
