26f3309fe04a4357326fd7365b65520bfd5a0dcd
Francesco Casamassa <francesco02@hotmail.ch>
2025-09-30 13:51:44 +0200

    basic implementation of g and h

---
+++ SW03/Aufgabe_1/src/main/java/hslu/oop/sw03/Pointer.java
@@ -0,0 +1,16 @@
+package hslu.oop.sw03;
+
+public class Pointer {
+
+    private int x;
+    private int y;
+
+    public Pointer(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getQuadrant(){
+        return 0;
+    }
+}
