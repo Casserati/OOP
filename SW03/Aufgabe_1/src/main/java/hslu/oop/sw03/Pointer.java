eb9e6e7ac09bee9dec2c9b5b6111929e3f5eece8
Francesco Casamassa <francesco02@hotmail.ch>
2025-09-30 14:15:32 +0200

    added implementation of g to i

--- SW03/Aufgabe_1/src/main/java/hslu/oop/sw03/Pointer.java
+++ SW03/Aufgabe_1/src/main/java/hslu/oop/sw03/Pointer.java
@@ -5,12 +5,20 @@ public class Pointer {
     private int x;
     private int y;
 
-    public Pointer(int x, int y){
+    public Pointer(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
-    public int getQuadrant(){
-        return 0;
+    public int getQuadrant() {
+        if (x < 0 && y > 0)
+            return 2;
+        if (x < 0 && y < 0)
+            return 3;
+        if (x > 0 && y > 0)
+            return 1;
+        if (x > 0 && y < 0)
+            return 4;
+        return 0; //falls der Point auf einer Achse ist
     }
 }
