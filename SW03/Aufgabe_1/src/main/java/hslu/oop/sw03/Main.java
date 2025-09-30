a46bdfc9918fd3a9c62ab1a958aef8500a344fa6
Francesco Casamassa <francesco02@hotmail.ch>
2025-09-30 13:03:16 +0200

    added new project for sw03

---
+++ SW03/Aufgabe_1/src/main/java/hslu/oop/sw03/Main.java
@@ -0,0 +1,17 @@
+package hslu.oop.sw03;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    private int max(int value1, int value2) {
+        return Math.max(value1, value2);
+    }
+
+    private int max(int value1, int value2, int value3) {
+        return Math.max(value1, Math.max(value2, value3));
+    }
+}
