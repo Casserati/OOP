b190baf366a65baf29cf68fe7718ebd6f32cee08
Francesco Casamassa <francesco02@hotmail.ch>
2025-10-21 13:03:52 +0200

    deleted oneliner

--- SW04/Aufgabe_3/src/main/java/hslu/oop/sw04/Main.java
+++ SW04/Aufgabe_3/src/main/java/hslu/oop/sw04/Main.java
@@ -13,7 +13,6 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println(line.getStartPoint().toString());
-        pointA.x = 100;
         System.out.println("Hello and welcome!");
 
         System.out.println(line.getStartPoint().toString());
