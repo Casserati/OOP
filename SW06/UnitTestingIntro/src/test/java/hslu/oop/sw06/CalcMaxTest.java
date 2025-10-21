9e0863a7d003e210f46a34c29e0bc12d81de084a
Francesco Casamassa <francesco02@hotmail.ch>
2025-10-21 16:57:47 +0200

    added all other test scenarios

--- SW06/UnitTestingIntro/src/test/java/hslu/oop/sw06/CalcMaxTest.java
+++ SW06/UnitTestingIntro/src/test/java/hslu/oop/sw06/CalcMaxTest.java
@@ -9,22 +9,70 @@ class CalcMaxTest {
     CalcMax calcMax = new CalcMax();
 
     @Test
-    void test_two_param_max_firstparam(){
-        int result = calcMax.max(2,1);
+    void test_twoParamMax_firstParam() {
+        int result = calcMax.max(2, 1);
 
         Assertions.assertEquals(2, result);
     }
 
     @Test
-    void test_two_param_max_secondparam(){
-        int result = calcMax.max(1,2);
+    void test_twoParamMax_secondParam() {
+        int result = calcMax.max(1, 2);
 
         Assertions.assertEquals(2, result);
     }
 
     @Test
-    void test_two_param_max_bothEquals(){
-        int result = calcMax.max(2,2);
+    void test_twoParamMax_bothEquals() {
+        int result = calcMax.max(2, 2);
+
+        Assertions.assertEquals(2, result);
+    }
+
+    @Test
+    void test_threeParamMax_firstParam() {
+        int result = calcMax.max(2, 1, 0);
+
+        Assertions.assertEquals(2, result);
+    }
+
+    @Test
+    void test_threeParamMax_secondParam() {
+        int result = calcMax.max(1, 2, 0);
+
+        Assertions.assertEquals(2, result);
+    }
+
+    @Test
+    void test_threeParamMax_thirdParam() {
+        int result = calcMax.max(0, 1, 2);
+
+        Assertions.assertEquals(2, result);
+    }
+
+    @Test
+    void test_threeParamMax_firstTwoEqual() {
+        int result = calcMax.max(1, 1, 0);
+
+        Assertions.assertEquals(1, result);
+    }
+
+    @Test
+    void test_threeParamMax_lastTwoEqual() {
+        int result = calcMax.max(0, 1, 1);
+
+        Assertions.assertEquals(1, result);
+    }
+
+    @Test
+    void test_threeParamMax_firstAndLastTwoEqual() {
+        int result = calcMax.max(1, 0, 1);
+
+        Assertions.assertEquals(1, result);
+    }
+    @Test
+    void test_threeParamMax_AllEqual() {
+        int result = calcMax.max(2, 2, 2);
 
         Assertions.assertEquals(2, result);
     }
