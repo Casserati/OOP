6a7a473ea5280adbce75191821f3e6e20f2d5400
Francesco Casamassa <francesco02@hotmail.ch>
2025-10-28 14:01:07 +0100

    done exercise 3

---
+++ SW07/Calculator/src/test/java/hslu/oop/sw06/CalculatorTest.java
@@ -0,0 +1,33 @@
+package hslu.oop.sw06;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    CalculatorImpl calculator = new CalculatorImpl();
+
+    @Test
+    void testAddition_twoZeros() {
+        Assertions.assertEquals(0, calculator.addition(0,0));
+    }
+
+    @Test
+    void testAddition_twoNegativeNumbers() {
+        Assertions.assertEquals(-8, calculator.addition(-3,-5));
+    }
+
+
+
+    @Test
+    void testAddition_twoOverflowNumbers() {
+        Assertions.assertEquals(-2147483648, calculator.addition(2147483647,1));
+    }
+
+    @Test
+    void testAddition_twoOverflowNumbersNegative() {
+        Assertions.assertEquals(2147483647, calculator.addition(-2147483648,-1));
+    }
+}
