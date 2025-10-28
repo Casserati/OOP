b5f5a1d03335dc9c012cf99f27497a5db87fa026
Francesco Casamassa <francesco02@hotmail.ch>
2025-10-28 14:14:50 +0100

    changed test params

--- SW06/UnitTestingIntro/src/test/java/hslu/oop/sw06/CalcMaxTest.java
+++ SW06/UnitTestingIntro/src/test/java/hslu/oop/sw06/CalcMaxTest.java
@@ -17,63 +17,63 @@ class CalcMaxTest {
 
     @Test
     void test_twoParamMax_secondParam() {
-        int result = calcMax.max(1, 2);
+        int result = calcMax.max(3, 4);
 
-        Assertions.assertEquals(2, result);
+        Assertions.assertEquals(4, result);
     }
 
     @Test
     void test_twoParamMax_bothEquals() {
-        int result = calcMax.max(2, 2);
+        int result = calcMax.max(5, 5);
 
-        Assertions.assertEquals(2, result);
+        Assertions.assertEquals(5, result);
     }
 
     @Test
     void test_threeParamMax_firstParam() {
-        int result = calcMax.max(2, 1, 0);
+        int result = calcMax.max(10, 9, 8);
 
-        Assertions.assertEquals(2, result);
+        Assertions.assertEquals(10, result);
     }
 
     @Test
     void test_threeParamMax_secondParam() {
-        int result = calcMax.max(1, 2, 0);
+        int result = calcMax.max(12, 13, 11);
 
-        Assertions.assertEquals(2, result);
+        Assertions.assertEquals(13, result);
     }
 
     @Test
     void test_threeParamMax_thirdParam() {
-        int result = calcMax.max(0, 1, 2);
+        int result = calcMax.max(14, 15, 16);
 
-        Assertions.assertEquals(2, result);
+        Assertions.assertEquals(16, result);
     }
 
     @Test
     void test_threeParamMax_firstTwoEqual() {
-        int result = calcMax.max(1, 1, 0);
+        int result = calcMax.max(18, 18, 17);
 
-        Assertions.assertEquals(1, result);
+        Assertions.assertEquals(18, result);
     }
 
     @Test
     void test_threeParamMax_lastTwoEqual() {
-        int result = calcMax.max(0, 1, 1);
+        int result = calcMax.max(19, 20, 20);
 
-        Assertions.assertEquals(1, result);
+        Assertions.assertEquals(20, result);
     }
 
     @Test
     void test_threeParamMax_firstAndLastTwoEqual() {
-        int result = calcMax.max(1, 0, 1);
+        int result = calcMax.max(22, 21, 22);
 
-        Assertions.assertEquals(1, result);
+        Assertions.assertEquals(22, result);
     }
     @Test
     void test_threeParamMax_AllEqual() {
-        int result = calcMax.max(2, 2, 2);
+        int result = calcMax.max(23, 23, 23);
 
-        Assertions.assertEquals(2, result);
+        Assertions.assertEquals(23, result);
     }
 }
