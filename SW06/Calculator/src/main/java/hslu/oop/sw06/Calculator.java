1987decbc1cc09abfd41d293e274f11dbddfbc32
Francesco Casamassa <francesco02@hotmail.ch>
2025-10-28 14:09:31 +0100

    changed folder for calculator project

---
+++ SW06/Calculator/src/main/java/hslu/oop/sw06/Calculator.java
@@ -0,0 +1,12 @@
+package hslu.oop.sw06;
+
+public interface Calculator {
+
+    /**
+     * addition method for calculating two integers together
+     * @param a
+     * @param b
+     * @return sum of a and b
+     */
+    int addition(int a, int b);
+}
---
+++ SW06/Calculator/src/main/java/hslu/oop/sw06/CalculatorImpl.java
@@ -0,0 +1,9 @@
+package hslu.oop.sw06;
+
+public class CalculatorImpl implements Calculator{
+
+    @Override
+    public int addition(int a, int b) {
+        return a + b;
+    }
+}
