9be6c727aa8c5e3207cfd810c1d2f3a45b493fde
Francesco Casamassa <francesco02@hotmail.ch>
2025-11-18 14:31:49 +0100

    done prerequirements

--- SW09/TemperaturRegler/src/main/java/hslu/oop/sw09/Main.java
+++ SW09/TemperaturRegler/src/main/java/hslu/oop/sw09/Main.java
@@ -1,17 +1,33 @@
 package hslu.oop.sw09;
 
+import hslu.oop.sw09.collections.TemperatureVerlaufImpl;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    private static final Logger LOGGER = LogManager.getLogger(Main.class);
+
+    private static TemperatureVerlaufImpl temperatureVerlauf = new TemperatureVerlaufImpl();
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Temperature in Celsius (or 'exit' to exit):");
+        String input = scanner.next();
+
+        if (!input.equals("exit")) {
+            try {
+                Temperature created = Temperature.createFromCelsius(Float.parseFloat(input));
+                LOGGER.info("Temperature created: {}", created);
+                temperatureVerlauf.add(created);
+                LOGGER.info("Temperature was successfully inserted into the list! Size now: {}", temperatureVerlauf.getCount());
+            } catch (IllegalArgumentException ex) {
+                LOGGER.error(ex.getMessage());
+            }
         }
     }
 }
--- SW10/TemperaturRegler/src/main/java/hslu/oop/sw10/Main.java
+++ SW10/TemperaturRegler/src/main/java/hslu/oop/sw10/Main.java
@@ -1,9 +1,33 @@
 package hslu.oop.sw10;
 
+import hslu.oop.sw10.collections.TemperatureVerlaufImpl;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
 
+    private static final Logger LOGGER = LogManager.getLogger(Main.class);
+
+    private static TemperatureVerlaufImpl temperatureVerlauf = new TemperatureVerlaufImpl();
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Temperature in Celsius (or 'exit' to exit):");
+        String input = scanner.next();
+
+        if (!input.equals("exit")) {
+            try {
+                Temperature created = Temperature.createFromCelsius(Float.parseFloat(input));
+                LOGGER.info("Temperature created: {}", created);
+                temperatureVerlauf.add(created);
+                LOGGER.info("Temperature was successfully inserted into the list! Size now: {}", temperatureVerlauf.getCount());
+            } catch (IllegalArgumentException ex) {
+                LOGGER.error(ex.getMessage());
+            }
+        }
     }
 }
