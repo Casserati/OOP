94457e02438b5633febf99e1783caf21844c2433
Francesco Casamassa <francesco02@hotmail.ch>
2025-12-02 13:03:30 +0100

    deleted import

--- SW11/TemperaturRegler/src/main/java/hslu/oop/sw11/csv/CsvReader.java
+++ SW11/TemperaturRegler/src/main/java/hslu/oop/sw11/csv/CsvReader.java
@@ -11,7 +11,6 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class CsvReader {
