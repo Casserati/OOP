6c689d1668c849bb7a220541d705e7de111f9ac0
Francesco Casamassa <francesco_casamassa@sluz.ch>
2026-01-15 23:36:49 +0100

    added uml diagram for MEP just to know what is what

---
+++ Vorbereitung/Uml/src/main/java/org/example/JavaClass.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.util.List;
+
+public class JavaClass implements InterfaceClass {
+
+    public static final String TEST = "test";
+
+    private static int number = 10;
+
+    private List<SecondClass> secondClassList;
+    private double decimal = 5L;
+    protected float floatNumber = 7.5f;
+    public byte aByte = 0;
+
+    public JavaClass() {}
+
+    public void testingMethod(SecondClass secondClass) {
+    }
+}
